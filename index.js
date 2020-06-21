d6b7320e31980678bef826ff0c29875bce39a7b0
Zoe <52415527+ztsamuels@users.noreply.github.com>
2020-06-21 14:41:52 -0700

    - made background image responsive - realigned name and nav - actually got name typing thing to work

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+// name typewriter code
+console.log("java ready");
 var i=0;
 var txt = 'zoe samuels'; // the text being typed
-var speed = 150; // speed of typing
+var speed = 160; // speed of typing
 
 function typewriter() {
   if (i < txt.length) {
-    document.getElementById("name").innerHTML +=charAt(i);
+    document.getElementById("zoe").innerHTML += txt.charAt(i);
     i++;
     setTimeout(typewriter,speed);
   }
+  console.log("name printed");
 }
+typewriter();
